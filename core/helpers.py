c2f401e376d7abe4a06b9af642c7826d92522618
CarlosNatanael <carlosnatha2345@gmail.com>
2025-12-24 20:46:28 -0300

    feat: adicionar suporte para RecallValue e novos métodos de conversão

--- core/helpers.py
+++ core/helpers.py
@@ -1,4 +1,4 @@
-from .value import MemoryValue
+from .value import MemoryValue, RecallValue
 from .constants import MemorySize
 
 def byte(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.BIT8)
@@ -14,10 +14,16 @@ def bit6(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.BI
 def bit7(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.BIT7)
 def low4(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.LOWER4)
 def high4(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.UPPER4)
+
 def float32(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.FLOAT)
+def float32be(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.FLOAT_BE)
+def double32(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.DOUBLE32)
 def bitcount(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.BITCOUNT)
 def mbf32(address: int) -> MemoryValue: return MemoryValue(address, MemorySize.MBF32)
 
 def prior(mem: MemoryValue) -> MemoryValue: return mem.prior()
 def delta(mem: MemoryValue) -> MemoryValue: return mem.delta()
 def bcd(mem: MemoryValue) -> MemoryValue:   return mem.bcd()
+def invert(mem: MemoryValue) -> MemoryValue: return mem.invert()
+
+def recall() -> RecallValue: return RecallValue()
