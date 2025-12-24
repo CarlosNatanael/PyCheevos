e58d603d3c063108505e6bfde540a41ec949202c
Srleo12 <carlosnatha2345@gmail.com>
2025-12-24 16:58:42 -0300

    error: correção de bug [não revolvido]

--- main.py
+++ main.py
@@ -3,36 +3,13 @@ from models.achievement import Achievement
 from core.helpers import byte
 from core.constants import Flag
 
-def dano_carro():
-    mem_dano = byte(0x000076)
-    mem_evento = byte(0x0007dd)
-    mem_verde = byte(0x00009e)
-    mem_colocacao = byte(0x0007d9)
-    mem_pista = byte(0x0013de)
-
-    cond_iniciar = (mem_verde == 0).with_hits(1)
-    pista_monaco = (mem_pista == 14)
-    cond_primeiro = (mem_colocacao == 0)
-
-    vitoria_cond = (mem_evento == 7)
-    vitoria_cond.flag = Flag.TRIGGER
-
-    delta_corrida = (mem_evento.delta() == 13)
-
-    core = [
-        cond_iniciar,
-        pista_monaco,
-        cond_primeiro,
-        vitoria_cond,
-        delta_corrida
-    ]
-
-    cond_reset = (mem_dano > 0)
-    cond_reset.flag = Flag.RESET_IF
-
-    alt_dano = [cond_reset]
-
-    return core, alt_dano
+def logica_matematica():
+    moedas_fase1 = byte(0x10)
+    moedas_fase2 = byte(0x20)
+    
+    soma_cond = (moedas_fase1 + moedas_fase2 > 50)
+    
+    return [soma_cond]
 
 meu_set = AchievementSet(game_id=23121, title="Racing game - No nitro")
 
@@ -43,10 +20,8 @@ conquista = Achievement(
     badge="00000"
 )
 
-l_core, l_alt1 = dano_carro()
-
-conquista.add_core(l_core)
-conquista.add_alt(l_alt1)
+l_math = logica_matematica()
+conquista.add_core(l_math)
 
 meu_set.add_achievement(conquista)
 meu_set.save()
