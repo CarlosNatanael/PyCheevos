d5b983715c34dcd38fe5d484dbb60c47ac3c888b
Srleo12 <carlosnatha2345@gmail.com>
2025-12-24 13:54:54 -0300

    test: atualizar lógica de conquistas e adicionar nova conquista "Dano Teste" para o circuito Monaco

--- main.py
+++ main.py
@@ -3,38 +3,50 @@ from models.achievement import Achievement
 from core.helpers import byte
 from core.constants import Flag
 
-def logica_sem_nitro():
-    mem_nitro = byte(0x0005c7)
+def dano_carro():
+    mem_dano = byte(0x000076)
     mem_evento = byte(0x0007dd)
-
+    mem_verde = byte(0x00009e)
+    mem_colocacao = byte(0x0007d9)
     mem_pista = byte(0x0013de)
 
-    reset_cond = (mem_nitro < 4)
-    reset_cond.flag = Flag.RESET_IF
+    cond_iniciar = (mem_verde == 0).with_hits(1)
+    pista_monaco = (mem_pista == 14)
+    cond_primeiro = (mem_colocacao == 0)
 
-    vitoria_cond = (mem_evento == 0x07).with_hits(1)
+    vitoria_cond = (mem_evento == 7)
+    vitoria_cond.flag = Flag.TRIGGER
 
-    core = [vitoria_cond, reset_cond]
+    delta_corrida = (mem_evento.delta() == 13)
 
-    alt_pista1 = [(mem_pista == 0x00)]
-    alt_pista2 = [(mem_pista == 0x01)]
+    core = [
+        cond_iniciar,
+        pista_monaco,
+        cond_primeiro,
+        vitoria_cond,
+        delta_corrida
+    ]
 
-    return core, alt_pista1, alt_pista2
+    cond_reset = (mem_dano > 0)
+    cond_reset.flag = Flag.RESET_IF
+
+    alt_dano = [cond_reset]
+
+    return core, alt_dano
 
 meu_set = AchievementSet(game_id=23121, title="Racing game - No nitro")
 
-conquista = Achievement (
-    title="Mestre da pista",
-    description="Vença no circuito Italiano sem usar nitro",
-    points=10,
+conquista = Achievement(
+    title="Dano Teste",
+    description="Vença no circuito Monaco sem nenhum dano",
+    points=25,
     badge="00000"
 )
 
-l_core, l_pista1, l_pista2 = logica_sem_nitro()
+l_core, l_alt1 = dano_carro()
 
 conquista.add_core(l_core)
-conquista.add_alt(l_pista1)
-conquista.add_alt(l_pista2)
+conquista.add_alt(l_alt1)
 
 meu_set.add_achievement(conquista)
 meu_set.save()
