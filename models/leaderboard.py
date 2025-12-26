a2b5b50283b7729eecea688cf1c3953cd8e29f42
CarlosNatanael <carlosnatha2345@gmail.com>
2025-12-26 19:39:29 -0300

    feat: permitir que condições únicas sejam passadas como argumento nas funções de configuração do leaderboard

--- models/leaderboard.py
+++ models/leaderboard.py
@@ -32,5 +32,37 @@ class Leaderboard:
         return flat_list
     
     def set_start(self, conditions: Union[Condition, List]):
-        if not isinstance(conditions, list):
-            print()
+        if not isinstance(conditions, list): conditions = [conditions]
+        self.start = self._flatten(conditions)
+        return self
+    
+    def set_cancel(self, conditions: Union[Condition, List]):
+        if not isinstance(conditions, list): conditions = [conditions]
+        self.cancel = self._flatten(conditions)
+        return self
+
+    def set_submit(self, conditions: Union[Condition, List]):
+        if not isinstance(conditions, list): conditions = [conditions]
+        self.submit = self._flatten(conditions)
+        return self
+    
+    def set_value(self, conditions: Union[Condition, List]):
+        if not isinstance(conditions, list): conditions = [conditions]
+        self.value = self._flatten(conditions)
+        return self
+    
+    def _render_group(self, conditions: List[Condition, List]): #type: ignore
+        return "_".join([c.render() for c in conditions])
+    
+    def render(self) -> str:
+        start_str = self._render_group(self.start)
+        cancel_str = self._render_group(self.cancel)
+        submit_str = self._render_group(self.submit)
+        value_str = self._render_group(self.value)
+
+        lower_val = "1" if self.lower_is_better else "0"
+
+        return (
+            f'L{self.id}:"{start_str}":"{cancel_str}":"{submit_str}":"{value_str}":'
+            f'{self.format.value}:{self.title}:{self.description}:{lower_val}'
+        )
