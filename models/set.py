6ff443e9391e18e03146b993ce2f2b34e327ff03
Player <redacteddoesit@gmail.com>
2025-12-27 16:28:02 +0000

    update: generate files to /output/game name - id

--- models/set.py
+++ models/set.py
@@ -1,4 +1,5 @@
 from typing import List
+from pathlib import Path
 from .achievement import Achievement
 from .leaderboard import Leaderboard
 from .rich_presence import RichPresence
@@ -23,19 +24,28 @@ class AchievementSet:
         self.rich_presence = rp
         return self
 
-    def save(self, path: str = None): # type: ignore
-        filename_user = path if path else f"{self.game_id}-User.txt"
-        with open(filename_user, "w", encoding="utf-8") as f:
+    def save(self, path: str | None = None): # type: ignore
+        if path is None:
+            root = Path(__file__).resolve().parent.parent
+            output = root / "output" / f"{self.title} - {self.game_id}"
+        else:
+            output = Path(path)
+
+        output.mkdir(exist_ok=True)
+        
+        user_file = output / f"{self.game_id}-User.txt"
+
+        with open(user_file, "w", encoding="utf-8") as f:
             f.write("1.0\n")
             f.write(f"{self.title}\n")
             for ach in self.achievements:
                 f.write(ach.render() + "\n")
             for lb in self.leaderboards:
                 f.write(lb.render() + "\n")
-        print(f"Arquivo User gerado: {filename_user}")
+        print(f"Generated file: {user_file}")
 
         if self.rich_presence:
-            filename_rp = f"{self.game_id}-Rich.txt"
-            with open(filename_rp, "w", encoding="utf-8") as f:
+            rp_file = output / f"{self.game_id}-Rich.txt"
+            with open(rp_file, "w", encoding="utf-8") as f:
                 f.write(self.rich_presence.render())
-            print(f"Arquivo Rich Presence gerado: {filename_rp}")
+            print(f"Generated Rich Presence file: {rp_file}")
