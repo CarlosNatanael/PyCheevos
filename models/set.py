5d0b7bf70dc5a7e4e41d74ac77f7b250aa33bed7
CarlosNatanael <carlosnatha2345@gmail.com>
2025-12-27 02:12:45 -0300

    feat: nova logica para gerar o aquivo Rich.txt

--- models/set.py
+++ models/set.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from .achievement import Achievement
 from .leaderboard import Leaderboard
+from .rich_presence import RichPresence
 
 class AchievementSet:
     def __init__(self, game_id: int, title: str):
@@ -8,6 +9,7 @@ class AchievementSet:
         self.title = title
         self.achievements: List[Achievement] = []
         self.leaderboards: List[Leaderboard] = []
+        self.rich_presence: RichPresence = None #type: ignore
 
     def add_achievement(self, achievement: Achievement):
         self.achievements.append(achievement)
@@ -15,6 +17,10 @@ class AchievementSet:
     
     def add_leaderboard(self, leaderboard: Leaderboard):
         self.leaderboards.append(leaderboard)
+        return self
+    
+    def add_rich_presence(self, rp: RichPresence):
+        self.rich_presence = rp
         return self
 
     def save(self, path: Optional[str] = None):
@@ -24,11 +30,14 @@ class AchievementSet:
         with open(filename, "w", encoding="utf-8") as f:
             f.write("1.0\n")
             f.write(f"{self.title}\n")
-
             for ach in self.achievements:
                 f.write(ach.render() + "\n")
-            
             for lb in self.leaderboards:
                 f.write(lb.render() + "\n")
-                
         print(f"Arquivo gerado: {filename}")
+
+        if self.rich_presence:
+            filename_rp = f"{self.game_id}-Rich.txt"
+            with open(filename_rp, "w", encoding="utf-88") as f:
+                f.write(self.rich_presence.render())
+            print(f"Arquivo Rich Presence gerado: {filename_rp}")
