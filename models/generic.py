4a1ddecb30d158da95da21f2633de5a215cdffb7
CarlosNatanael <carlosnatha2345@gmail.com>
2025-12-28 18:24:47 -0300

    feat: A class for handling both static addresses and pointers.

---
+++ models/generic.py
@@ -0,0 +1,12 @@
+from core.helpers import byte, word, dword
+
+class GameObject:
+    def __init__(self, address):
+        self.base_address = address
+
+    def offset(self, offset_val: int, type_func=byte):
+        if isinstance(self.base_address, int):
+            return type_func(self.base_address + offset_val)
+        
+        return self.base_address >> type_func(offset_val)
+    
